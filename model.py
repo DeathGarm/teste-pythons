8eff050a3028620dec00674da4651e50eda11e41
Jeremy Guimin <guiminjeremy@gmail.com>
2022-01-07 17:44:34 +0100

    nouvelle correction

--- model.py
+++ model.py
@@ -39,46 +39,50 @@ class Zone:
     @property
     def population(self):
         return len(self.inhabitants)
-        
-    def add_inhabitants(self, inhabitan):
-        self.inhabitants.append(self)
-    
-    def contrains(self, position):
-        return position.longitude >= min(self.corner1.longitude,self.corner2.longitude) and \
-            position.longitude < max(self.corner1.longitude,self.corner2.longitude) and \
-            position.longitude >= min(self.corner1.latitude,self.corner2.latitude) and \
-            position.longitude < max(self.corner1.latitude,self.corner2.latitude)
+
+    def add_inhabitant(self, inhabitant):
+        self.inhabitants.append(inhabitant)
+
+    def contains(self, position):
+        return position.longitude >= min(self.corner1.longitude, self.corner2.longitude) and \
+            position.longitude < max(self.corner1.longitude, self.corner2.longitude) and \
+            position.latitude >= min(self.corner1.latitude, self.corner2.latitude) and \
+            position.latitude < max(self.corner1.latitude, self.corner2.latitude)
 
     @classmethod
-    def find_zone_that_contrains(cls, position):
+    def find_zone_that_contains(cls, position):
         longitude_index = int((position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES)/ cls.WIDTH_DEGREES)
         latitude_index = int((position.latitude_degrees - cls.MIN_LATITUDE_DEGREES)/ cls.HEIGHT_DEGREES)
-        longitude_bins = int((cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES)
+        longitude_bins = int((cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES) # 180-(-180) / 1
         zone_index = latitude_index * longitude_bins + longitude_index
 
         zone = cls.ZONES[zone_index]
-        assert zone.contrains(position)
-        
+        assert zone.contains(position)
+
         return zone
 
     @classmethod
     def initialize_zones(cls):
-        for latitude in range (cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
+        cls.ZONES = []
+        for latitude in range(cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
             for longitude in range(cls.MIN_LONGITUDE_DEGREES, cls.MAX_LONGITUDE_DEGREES, cls.WIDTH_DEGREES):
                 bottom_left_corner = Position(longitude, latitude)
                 top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
                 zone = Zone(bottom_left_corner, top_right_corner)
                 cls.ZONES.append(zone)
 
+
+
+
 def main():
     Zone.initialize_zones()
     for agent_attributes in json.load(open("agents-100k.json")):
         latitude = agent_attributes.pop("latitude")
         longitude = agent_attributes.pop("longitude")
-        position = Position(latitude, longitude)
+        position = Position(longitude, latitude)
         agent = Agent(position, **agent_attributes)
-        zone = Zone.find_zone_that_contrains(position)
+        zone = Zone.find_zone_that_contains(position)
         zone.add_inhabitant(agent)
-        print("Zone population: ", zone.population)
+        print(zone.population)
 
 main()
